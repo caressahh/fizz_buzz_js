60837db96e9a7b0182d3930b67eeee7f5639ce73
Carissa <carissa@edgren.nu>
2017-06-22 14:55:59 +0200

    fourth test is green

--- assets/js/fizz_buzz.js
+++ assets/js/fizz_buzz.js
@@ -1,11 +1,14 @@
 function FizzBuzz() {}
 
 FizzBuzz.prototype.getValue = function(num) {
-  if (num % 3 === 0){
-    return 'fizz';
+  if (num % 15 === 0){
+    return 'fizzbuzz';
   }
   else if (num % 5 === 0){
     return 'buzz';
+  }
+  else if (num % 3 === 0){
+  return 'fizz';
   }
   return num;
   };
