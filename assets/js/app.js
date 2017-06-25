2f30032a2de05b904d6b02291e2977748ed21492
Carissa <carissa@edgren.nu>
2017-06-25 15:56:23 +0200

    adds foundation for html page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,5 @@
 
-var onReadyEvents = function(){
-  $('#click_me').click(function() {
-    var text = $('#text').val();
-    $('#display_message').html(text);
+$(document).ready(function () {
+  $('#start').click(function() {
   });
-}
+});
